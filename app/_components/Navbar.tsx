1a85583c5e6ed5f2b6a7f4dea0aa2151fba0850a
Mohammed Tahar <mouh.bel2456@gmail.com>
2024-04-15 20:55:48 +0000

    comitted changes

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,21 +1,22 @@
 import Link from "next/link";
-import Image from "next/image";
+import Image from 'next/image'; // Import if using Next.js 10 or below
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-800 flex justify-between  relative z-30 py-5  max-container ">
-      <div className="flex justify-evenly">
-        
-        <Link href="/">
+    <nav className="border-2 border-red-800 flexBetween gap-12 relative  py-5 max-container">
+      <div className="flex-1 flexStart gap-10 " >
+      <Link href="/">
           <Image src="/eurobridg.jpg" alt="logo" width={140} height={29} />
         </Link>
-       
-      
-       <ul className="hidden h-full gap-12  ">
-        <Link href="/" className="regular-16 text-gray-50 flex justify-center cursor-pointer pb-1.5 transition-all font-bold"> brows companies </Link>
+
+        <ul className=" h-full gap-12 xl:flex hidden text-small ">
+           < a  className="regular-16 text-[#515B6F] flex justify-center cursor-pointer pb-1.5 transition-all hover:text-[#4640DE] " href="/">
+           brows companies
+          </a>
+          
         </ul>
-       </div>
-     
+      </div>
+      
       
     </nav>
   );
