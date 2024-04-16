c92ad7e5f75ffa87a9d7983741c3b2248c04c3cd
Mohammed Tahar <mouh.bel2456@gmail.com>
2024-04-16 23:09:05 +0000

    commited changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,10 @@
 import Image from "next/image";
 import { Open_Sans } from 'next/font/google';
+import Herosection from "./_components/Herosection";
 
 export default function Home() {
-  return <main className=" flex flex-col bg-[#F8F8FD]"> 
-  <div className="flex flex-row justify-around ml-20  ">
-    <div className={`flex flex-col ml-5 mr-0 my-10 font-semibold`}>
-    <p className="text-[55px] leading-[4.5rem] mt-10 font-bold font-body w-1/3" >
-      Where <span className="text-[#26A4FF]"> <br/>Trainees </span> And <span className="text-[#26A4FF]">Traineeships </span> <span className="text-[#4640DE]" >Meet</span>
-    </p>
-    <p className="text-[#515B6F] mt-4">Launch Your European Career: Connect with Top  <br />Companies & Ignite Your Potential.</p>
-    </div>
-    <div>
-       <Image src="/image.jpg" alt="logo" width={1000} height={700} className="rounded-l-[50px] mt-8 " />
-    </div>
-   
-        
-  </div>
-  <div >search</div>
-
-
+  return <main > 
+  <Herosection/>
   
   </main>;
 }
