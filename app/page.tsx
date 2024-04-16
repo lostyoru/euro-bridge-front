f8ba9bd6b25b89a6195bef0cd6935a098a4c8749
Mohammed Tahar <mouh.bel2456@gmail.com>
2024-04-16 01:43:51 +0000

    comitted changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return <main className=" flex flex-col bg-[#F8F8FD]"> 
   <div className="flex flex-row justify-evenly ml-10 ">
     <div className="flex flex-col mx-20 my-10">
-    <h1 className="text-[65px] leading-[4.5rem] mt-10" >
+    <h1 className="text-[65px] leading-[4.5rem] mt-10 font-bold" >
       Where <strong className="text-[#26A4FF]"> <br/>Trainees</strong> And <strong className="text-[#26A4FF]">Traineeships  </strong> <strong className="text-[#4640DE]" >Meet</strong>
     </h1>
     <p className="text-[#515B6F] mt-4">Launch Your European Career: Connect with Top  <br />Companies & Ignite Your Potential.</p>
