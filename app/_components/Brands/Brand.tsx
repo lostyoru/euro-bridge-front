2d896f13660e26b6c89260af9a162a62679fabf4
Mohammed Tahar <mouh.bel2456@gmail.com>
2024-04-17 18:28:12 +0000

    comitted changes

--- app/_components/Brands/Brand.tsx
+++ app/_components/Brands/Brand.tsx
@@ -6,11 +6,10 @@ import React from "react";
 function Brand() {
   return (
     <div className=" flex flex-col justify-center items-center  ">
-     
       <div>
-      <div className=" mt-10   ml-20 text-[#75767b] ">
-        <p> Companies we helped grow</p>
-      </div>
+        <div className=" mt-10   ml-20 text-[#75767b] ">
+          <p> Companies we helped grow</p>
+        </div>
         <div className=" my-6 grid grid-cols-5 gap-1  ">
           <div className=" flex justify-center items-center flex-1 max-w-[150px] min-w-[120px] mx-[4rem] ">
             <Image src={disney} alt={"disney"} />
